93486167c44aea51d7b6522beca5cdddd6784d7b
Rohan Raj Gautam <rohanrajgautam@gmail.com>
2024-03-12 12:11:41 +0545

    chore: add button with external link in main page

--- pages/index.jsx
+++ pages/index.jsx
@@ -28,7 +28,17 @@ export default function HomePage({ email }) {
 						</Link>
 					</div>
 
-					<div>
+					<div className='w-full flex flex-col items-center justify-center mt-8'>
+						<div className='w-full h-full text-center mb-3'>
+							<a
+								href='https://cciwi-my.sharepoint.com/:x:/p/mike_foss/EfynUbpvRbZCr-pl55_jzIMBxfAHYSm37IPON9X0n-APSg?e=whbBHw'
+								target='_blank'
+								class='text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-1 focus:ring-indigo-300 font-medium rounded-xl text-sm px-5 py-2.5 dark:bg-indigo-600 dark:hover:bg-indigo-700 focus:outline-none dark:focus:ring-indigo-800'
+							>
+								CCI CapEx Report
+							</a>
+						</div>
+
 						<Image src='/cci-sample.jpeg' width={1297} height={1000} />
 					</div>
 				</>
